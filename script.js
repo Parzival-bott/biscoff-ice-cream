fe6eb5492f04d53d0db9130cf563366f36f176ae
Parzival-bott <siddharthshah421@gmail.com>
2026-02-08 22:26:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (currentSlide === 1) startTyping();
 
     // confetti only on FINAL slide (index 3)
-    if (currentSlide === 1 || currentSlide === 2 || currentSlide === 3) startConfetti();
+    if (currentSlide !=0) startConfetti();
     else stopConfetti();
   }
 
@@ -115,4 +115,3 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
-
